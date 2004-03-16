58e8dc696c2cf39a6542e49b3193a3b38daa89f8
walker <walker>
2004-03-16 18:27:53 +0000

    create nested loops, use seperate query for each location visited on a trip, decide seperately whether to divide by taxo for each location

--- tripdetail.php
+++ tripdetail.php
@@ -10,8 +10,7 @@ $whereClause = "sighting.LocationName=location.Name and species.Abbreviation=sig
 // total species countv for this trip
 $tripSpeciesCount = getFancySpeciesCount($whereClause);
 
-// get list of species, ordered first by location name then taxo order; retrieve location name as additional field
-$tripSpeciesQuery = getFancySpeciesQuery($whereClause, "sighting.LocationName, species.objectID", ", sighting.LocationName, location.objectid as locationid, sighting.Notes, sighting.Exclude, sighting.objectid as sightingid");
+$locationListQuery = performQuery("select distinct(location.objectid), location.Name from location, sighting where location.Name=sighting.LocationName and sighting.TripDate='". $tripInfo["Date"] . "'");
 
 $firstSightings = getFirstSightings();
 $firstYearSightings = getFirstYearSightings(substr($tripInfo["Date"], 0, 4));
@@ -43,32 +42,45 @@ $firstYearSightings = getFirstYearSightings(substr($tripInfo["Date"], 0, 4));
 
 <?php
 
-// Show what we got
-while($speciesInfo = mysql_fetch_array($tripSpeciesQuery)) {
-  if (strcmp($prevInfo["LocationName"], $speciesInfo["LocationName"]))
-  {
-    echo "<div class=\"titleblock\"><a href=\"./locationdetail.php?id=" . $speciesInfo["locationid"] . "\">" . $speciesInfo["LocationName"] . "</a></div>";
-  }
+while($locationInfo = mysql_fetch_array($locationListQuery))
+{
+	$tripLocationQuery = performQuery("select species.CommonName, species.objectid as speciesid, sighting.* from species, sighting where sighting.SpeciesAbbreviation=species.Abbreviation and sighting.TripDate='". $tripInfo["Date"] . "' and sighting.LocationName='" . $locationInfo["Name"] . "' order by species.objectid");
+	$tripLocationCount = performCount("select count(distinct SpeciesAbbreviation) from sighting where sighting.TripDate='". $tripInfo["Date"] . "' and sighting.LocationName='" . $locationInfo["Name"] . "'");
+	$divideByTaxo = ($tripLocationCount > 30);
 
-  echo "<div class=firstcell><a href=\"./speciesdetail.php?id=" . $speciesInfo["objectid"] . "\">" . $speciesInfo["CommonName"] . "</a>";
+	echo "<div class=\"titleblock\"><a href=\"./locationdetail.php?id=" . $locationInfo["objectid"] . "\">" . $locationInfo["Name"] . "</a>, " . $tripLocationCount . " species</div>";
+	echo "<div style=\"padding-left: 20px\">";
+	
+	while($info = mysql_fetch_array($tripLocationQuery))
+	{
+		$orderNum =  floor($info["objectid"] / pow(10, 9));
+		
+		if ($divideByTaxo && (getBestTaxonomyID($prevInfo["speciesid"]) != getBestTaxonomyID($info["speciesid"])))
+		{
+			$taxoInfo = getBestTaxonomyInfo($info["speciesid"]);
+			echo "<div class=\"titleblock\">" . $taxoInfo["CommonName"] . "</div>";
+		}
 
-  if (strlen($speciesInfo["Notes"]) > 0) {
-    echo "<div class=sighting-notes>" . $speciesInfo["Notes"] . "</div>";
-  }
+		echo "<div class=firstcell><a href=\"./speciesdetail.php?id=".$info["speciesid"]."\">".$info["CommonName"]."</a></div>";
 
-  if ($speciesInfo["Exclude"] == "1") {
-    echo "<div class=sighting-notes>excluded</div>";
-  }
+		if (strlen($info["Notes"]) > 0) {
+			echo "<div class=sighting-notes>" . $info["Notes"] . "</div>";
+		}
+ 
+		if ($info["Exclude"] == "1") {
+			echo "<div class=sighting-notes>excluded</div>";
+		}
+ 
+		$sightingID = $info["objectid"];
+ 
+		if ($firstSightings[$sightingID] != null) echo "<div class=sighting-notes>first life sighting</div>";
+		else if ($firstYearSightings[$sightingID] != null) echo "<div class=sighting-notes>first " . $tripYear . " sighting</div>";
+		
+		$prevInfo = $info;
+	}
 
-  $sightingID = $speciesInfo["sightingid"];
-
-  if ($firstSightings[$sightingID] != null) echo "<div class=sighting-notes>first life sighting</div>";
-  else if ($firstYearSightings[$sightingID] != null) echo "<div class=sighting-notes>first " . $tripYear . " sighting</div>";
-
-
-  echo "</div>";
-
-  $prevInfo = $speciesInfo;
+	echo "</div>";
+	
 }
 
 ?>
