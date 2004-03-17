6bf470ba61deccb8a8af86d3817d94663b559584
walker <walker>
2004-03-17 02:26:31 +0000

    rename stateindex.php to statedetail.php

--- statedetail.php
+++ statedetail.php
@@ -26,7 +26,31 @@ $stateListQuery = getFancySpeciesQuery($whereClause);
         <div class=pagesubtitle> <?php echo $stateListCount ?> species</div>
       </div>
 		
-<?php formatSpeciesList($stateListCount, $stateListQuery); ?>
+<?php
+
+	$divideByTaxo = ($stateListCount > 30);
+	
+	while($info = mysql_fetch_array($stateListQuery))
+	{
+		$orderNum =  floor($info["objectid"] / pow(10, 9));
+		
+		if ($divideByTaxo && (getBestTaxonomyID($prevInfo["objectid"]) != getBestTaxonomyID($info["objectid"])))
+		{
+			$taxoInfo = getBestTaxonomyInfo($info["objectid"]);
+			echo "<div class=\"titleblock\">" . $taxoInfo["CommonName"] . "</div>";
+		}
+
+		echo "<div class=firstcell><a href=\"./speciesdetail.php?id=".$info["objectid"]."\">".$info["CommonName"]."</a></div>";
+
+		if (strlen($info["Notes"]) > 0) {
+			echo "<div class=sighting-notes>" . $info["Notes"] . "</div>";
+		}
+		
+		$prevInfo = $info;
+	}
+
+
+?>
 
     </div>
   </body>
