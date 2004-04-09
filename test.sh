a573cf3ddebbc6cd1ebbf125068646dbe1e40c5d
walker <walker>
2004-04-09 17:51:50 +0000

    add new jsp's to testing script

--- test.sh
+++ test.sh
@@ -2,9 +2,11 @@
 
 echo "birdwalker" && (php ./birdwalker.php || (echo "birdwalker ERROR" && exit 1))
 echo "tripindex" && php ./tripindex.php | grep -q error && echo "tripindex ERROR" && exit 1
+echo "tripcreate" && php ./tripcreate.php | grep -q error && echo "tripcreate ERROR" && exit 1
 echo "speciesindex" && php ./speciesindex.php | grep -q error && echo "speciesindex ERROR" && exit 1
 echo "locationindex" && php ./locationindex.php | grep -q error && echo "locationindex ERROR" && exit 1
 echo "countyindex" && php ./countyindex.php | grep -q error && echo "countyindex ERROR" && exit 1
+echo "photoindex" && php ./photoindex.php | grep -q error && echo "photoindex ERROR" && exit 1
 echo "speciesdetail" && curl -s http://localhost/~walker/birdwalker2/speciesdetail.php?id=3020060500 | grep error && echo "speciesdetail ERROR" && exit 1
 echo "tripdetail" && curl -s http://localhost/~walker/birdwalker2/tripdetail.php?id=45 | grep error && echo "tripdetail ERROR" && exit 1
 echo "locationdetail" && curl -s http://localhost/~walker/birdwalker2/locationdetail.php?id=36 | grep error && echo "locationdetail ERROR" && exit 1
@@ -13,7 +15,8 @@ echo "orderdetail" && curl -s http://localhost/~walker/birdwalker2/orderdetail.p
 echo "countydetail" && curl -s http://localhost/~walker/birdwalker2/countydetail.php?county=Imperial | grep error && echo "countydetail ERROR" && exit 1
 echo "statedetail" && curl -s http://localhost/~walker/birdwalker2/statedetail.php?state=CA | grep error && echo "statedetail ERROR" && exit 1
 echo "yeardetail" && curl -s http://localhost/~walker/birdwalker2/yeardetail.php?year=1999 | grep error && echo "yeardetail ERROR" && exit 1
-
+echo "photodetail" && curl -s http://localhost/~walker/birdwalker2/photodetail.php?id=7861 | grep error && echo "photodetail.ERROR" && exit 1
+echo "sightingdetail" && curl -s http://localhost/~walker/birdwalker2/sightingdetail.php?id=7861 | grep error && echo "sightingdetail.ERROR" && exit 1
 echo "everything ok"
 
 exit 0
