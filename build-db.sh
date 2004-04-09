a63ac2987cf3ec23eefb21ec7c96048a744cfe06
walker <walker>
2004-04-09 17:54:45 +0000

    scripts for creating sql datagbase and dumping sql database to text

---
+++ build-db.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+cat birdwalker.sql trip.sql location.sql species.sql sighting.sql taxonomy.sql| /usr/local/mysql/bin/mysql -u birdwalker -p birdwalker
+
+
+
---
+++ dump-db.sh
@@ -0,0 +1,5 @@
+mysqldump -u birdwalker -pbirdwalker birdwalker location | grep INSERT > location.sql
+mysqldump -u birdwalker -pbirdwalker birdwalker sighting | grep INSERT > sighting.sql
+mysqldump -u birdwalker -pbirdwalker birdwalker trip | grep INSERT > trip.sql
+mysqldump -u birdwalker -pbirdwalker birdwalker species | grep INSERT > species.sql
+mysqldump -u birdwalker -pbirdwalker birdwalker taxonomy | grep INSERT > taxonomy.sql
