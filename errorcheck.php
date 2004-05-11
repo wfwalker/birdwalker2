24a73b1fb27aef90bb15f6c62fa75763651f8f7f
walker <walker>
2004-05-11 15:55:51 +0000

    new page for finding bogus abbreviations, etc.

---
+++ errorcheck.php
@@ -0,0 +1,37 @@
+
+<?php
+
+require("./birdwalker.php");
+
+$badAbbrevs = performQuery("select species.*,sighting.*, sighting.objectid as sightingid from sighting left join species on species.Abbreviation=sighting.SpeciesAbbreviation order by species.CommonName, sighting.SpeciesAbbreviation");
+
+?>
+
+<html>
+  <head>
+    <link title="Style" href="./stylesheet.css" type="text/css" rel="stylesheet">
+    <title>birdWalker | Bad Records</title>
+  </head>
+  <body>
+
+<?php navigationHeader() ?>
+
+    <div class=contentright>
+      <div class="titleblock">	  
+	  <div class=pagetitle>Bad Records</div>
+      </div>
+
+<?php
+while($sightingInfo = mysql_fetch_array($badAbbrevs)) {
+	if ($sightingInfo["CommonName"] == "") {
+		echo "<a href=\"./sightingedit.php?id=" . $sightingInfo["sightingid"] . "\">";
+		echo $sightingInfo["SpeciesAbbreviation"];
+		echo " " . $sightingInfo["LocationName"];
+		echo " " . $sightingInfo["TripDate"] . "</a><br>";
+	}
+	else
+	{
+		break;
+	}
+}
+?>
