03c238487b9899a7363ed52e0786168481bd662b
walker <walker>
2004-05-11 15:50:33 +0000

    start using mysql_num_rows, now uses one fewer query count excluded species in the trip species count

--- tripdetail.php
+++ tripdetail.php
@@ -8,16 +8,17 @@ $tripYear = substr($tripInfo["Date"], 0, 4);
 $tripCount = getTripCount();
 $whereClause = "sighting.LocationName=location.Name and species.Abbreviation=sighting.SpeciesAbbreviation and sighting.TripDate='" . $tripInfo["Date"]. "'";
 
-// total species countv for this trip
-$tripSpeciesCount = getFancySpeciesCount($whereClause);
-
 $locationListQuery = performQuery("select distinct(location.objectid), location.Name from location, sighting where location.Name=sighting.LocationName and sighting.TripDate='". $tripInfo["Date"] . "'");
 
 $firstSightings = getFirstSightings();
 $firstYearSightings = getFirstYearSightings(substr($tripInfo["Date"], 0, 4));
 
 // how many first sightings were on this trip?
-$tripSightings = performQuery("select objectid from sighting where TripDate='" . $tripInfo['Date'] . "' and Exclude!='1'");
+$tripSightings = performQuery("select objectid from sighting where TripDate='" . $tripInfo['Date'] . "'");
+
+// total species count for this trip
+$tripSpeciesCount = mysql_num_rows($tripSightings);
+
 $tripFirstSightings = 0;
 while($sightingInfo = mysql_fetch_array($tripSightings)) {
 	if ($firstSightings[$sightingInfo['objectid']] != null) { $tripFirstSightings++; }
@@ -40,9 +41,14 @@ while($sightingInfo = mysql_fetch_array($tripSightings)) {
       <div class=pagetitle> <?php echo $tripInfo["Name"] ?> Trip List</div>
       <div class=pagesubtitle> <?php echo $tripInfo["niceDate"] ?></div>
       <div class=metadata>Led by  <?php echo $tripInfo["Leader"] ?></div>
-<?php if (strlen($tripInfo["ReferenceURL"]) > 0) {
-      echo "<div><a href=\"" . $tripInfo["ReferenceURL"] . "\">See also...</a></div>";
-} ?>
+<?php
+if (strlen($tripInfo["ReferenceURL"]) > 0) {
+    echo "<div><a href=\"" . $tripInfo["ReferenceURL"] . "\">See also...</a></div>";
+}
+if (getEnableEdit()) {
+	echo "<div><a href=\"./tripedit.php?id=" . $tripID . "\">edit</a></div>";
+}
+?>
     </div>
 
       <div class=sighting-notes> <?php echo $tripInfo["Notes"] ?></div>
@@ -54,7 +60,7 @@ while($sightingInfo = mysql_fetch_array($tripSightings)) {
 while($locationInfo = mysql_fetch_array($locationListQuery))
 {
 	$tripLocationQuery = performQuery("select species.CommonName, species.objectid as speciesid, sighting.* from species, sighting where sighting.SpeciesAbbreviation=species.Abbreviation and sighting.TripDate='". $tripInfo["Date"] . "' and sighting.LocationName='" . $locationInfo["Name"] . "' order by species.objectid");
-	$tripLocationCount = performCount("select count(distinct SpeciesAbbreviation) from sighting where sighting.TripDate='". $tripInfo["Date"] . "' and sighting.LocationName='" . $locationInfo["Name"] . "'");
+	$tripLocationCount = mysql_num_rows($tripLocationQuery);
 	$divideByTaxo = ($tripLocationCount > 30);
 
 	echo "<div class=\"titleblock\"><a href=\"./locationdetail.php?id=" . $locationInfo["objectid"] . "\">" . $locationInfo["Name"] . "</a>, " . $tripLocationCount . " species</div>";
