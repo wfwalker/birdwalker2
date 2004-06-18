2f3f8b55e4860e1a24127fccf6bd3cf69f363996
walker <walker>
2004-06-18 02:10:48 +0000

    also upload the stylesheet

--- upload-php.sh
+++ upload-php.sh
@@ -2,4 +2,4 @@
 
 filesToCopy=$(find . -type f -name \*.php -not -name \*edit\* -and -not -name speciesindex2.php -and -not -name photosneeded.php -and -not -name targetyearbirds.php -and -not -name \*create\*)
 
-scp $filesToCopy walker@sven.spflrc.org:~/.www/
+scp stylesheet.css $filesToCopy walker@sven.spflrc.org:~/.www/
