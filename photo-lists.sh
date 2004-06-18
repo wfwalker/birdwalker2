a0a1394f0cd1961db6c3d01afeeef59542beb26e
walker <walker>
2004-06-18 02:08:25 +0000

    now looks for diffs in both the photos folder and the thumbs folder

--- photo-lists.sh
+++ photo-lists.sh
@@ -1,5 +1,8 @@
 echo "select concat('images/photo/', TripDate, '-', SpeciesAbbreviation, '.jpg') from sighting where Photo='1'" | mysql -u birdwalker -pbirdwalker birdwalker | sort > databaselist.txt
+echo "select concat('images/thumb/', TripDate, '-', SpeciesAbbreviation, '.jpg') from sighting where Photo='1'" | mysql -u birdwalker -pbirdwalker birdwalker | sort > databaselist2.txt
 
 find images/photo | sort > folderlist.txt
+find images/thumb | sort > folderlist2.txt
 
 diff --ignore-case databaselist.txt folderlist.txt
+diff --ignore-case databaselist2.txt folderlist2.txt
