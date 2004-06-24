7539ae675f06f86fe1f7112e60333239985c6f5f
walker <walker>
2004-06-24 19:00:40 +0000

    fix up titles, etc

--- speciesindex.php
+++ speciesindex.php
@@ -10,21 +10,22 @@ $speciesQuery = getSpeciesQuery();
 <html>
   <head>
     <link title="Style" href="./stylesheet.css" type="text/css" rel="stylesheet">
-    <title>birdWalker | Speciess</title>
+    <title>birdWalker | Species</title>
   </head>
   <body>
 
 <?php
 globalMenu();
 disabledBrowseButtons();
-navTrailBirds("list | <a href=\"./speciesindexbyyear.php\">by year</a>");
+navTrailBirds();
 pageThumbnail("select *, rand() as shuffle from sighting where Photo='1' order by shuffle");
 ?>
 
     <div class=contentright>
       <div class="titleblock">	  
-	  <div class=pagetitle>Species Index</div>
+	  <div class=pagetitle>Species</div>
 	  <div class=pagesubtitle><?php echo mysql_num_rows($speciesQuery) ?> Species</div>
+	  <div class=metadata>list | <a href="./speciesindexbyyear.php">by year</a></div>
       </div>
 
 <?php formatTwoColumnSpeciesList($speciesQuery); ?>
