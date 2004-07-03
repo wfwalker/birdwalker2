08ccb900af81ca159e71bad07942db01d9a79562
walker <walker>
2004-07-03 06:27:18 +0000

    add URL for John Cang

--- trip.sql
+++ trip.sql
@@ -308,7 +308,7 @@ INSERT INTO trip VALUES (307,'Mary','','Backyard','','2004-04-30');
 INSERT INTO trip VALUES (308,'FWS','','International Migratory Bird Day preview','','2004-05-18');
 INSERT INTO trip VALUES (309,'Mary and Bill','','Mount Hamilton','','2004-05-15');
 INSERT INTO trip VALUES (310,'Mary and Bill','','Strolling around the Zoo','','2004-05-08');
-INSERT INTO trip VALUES (311,'John Cang','','Bird Photography Workshop','Spent a very pleasant early morning and late afternoon shooting shorebirds under the tutelage of John Cang, talking about metering, fill flash, proper exposure for black and white birds, flight shots, and so on. Highly recommended.','2004-05-23');
+INSERT INTO trip VALUES (311,'John Cang','http://www.johncangphoto.com/','Bird Photography Workshop','Spent a very pleasant early morning and late afternoon shooting shorebirds under the tutelage of John Cang, talking about metering, fill flash, proper exposure for black and white birds, flight shots, and so on. Highly recommended.','2004-05-23');
 INSERT INTO trip VALUES (312,'Bill Walker','','Photo Safari','','2004-05-25');
 INSERT INTO trip VALUES (313,'Bill Walker','','Photo Safari','','2004-05-19');
 INSERT INTO trip VALUES (314,'Mary and Bill','','Saturday Stroll','','2002-07-27');
