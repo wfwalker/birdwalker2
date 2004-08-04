4abbc46149b05ca60bbe48724c9115c174f2b3ba
walker <walker>
2004-08-04 01:27:35 +0000

    new indenting style indent SQL queries

--- countyindex.php
+++ countyindex.php
@@ -8,7 +8,11 @@
   </head>
   <body>
 
-<?php globalMenu(); disabledBrowseButtons(); ?>
+<?php
+globalMenu();
+disabledBrowseButtons();
+pageThumbnail("SELECT *, rand() AS shuffle FROM sighting WHERE Photo='1' ORDER BY shuffle");
+?>
 
 <div class=navigationright><a href="./index.php">birdWalker</a> &gt; <a href="./locationindex.php">locations</a> &gt; counties by year</div>
 
@@ -25,12 +29,17 @@ $yearArray = null;
 $lastStateAccumulated = "NONE";
 $prevState = "NONE";
 $countyToAccumulate = "NONE";
-$countyStats = performQuery("select location.County, location.State, location.objectid, count(distinct sighting.SpeciesAbbreviation) as SpeciesCount, year(sighting.TripDate) as theyear from location, sighting where sighting.LocationName=location.Name group by location.County, theyear order by State, County, theyear");
+$countyStats = performQuery("
+    SELECT location.County, location.State, location.objectid, COUNT(DISTINCT sighting.SpeciesAbbreviation) AS
+      SpeciesCount, year(sighting.TripDate) AS theyear
+      FROM location, sighting
+      WHERE sighting.LocationName=location.Name
+      GROUP BY location.County, theyear
+      ORDER BY State, County, theyear"); ?>
 
-?>
     <tr><td></td><? insertYearLabels(); ?></tr>
-<?
 
+<?
 while ($info = mysql_fetch_array($countyStats))
 {
 	$county = $info["County"];
@@ -39,36 +48,37 @@ while ($info = mysql_fetch_array($countyStats))
 	$speciesCount = $info["SpeciesCount"];
 
 	if ($lastStateAccumulated != $prevState)
-	{
-		echo "<tr><td colspan=11 class=heading><a href=\"statespecies.php?state=" . $prevState . "\">" . getStateNameForAbbreviation($prevState) . "</a></td></tr>";
-		$lastStateAccumulated = $prevState;
+	{ ?>
+		<tr>
+            <td colspan=11 class=heading>
+                <a href="statespecies.php?state=<?= $prevState ?>"><?= getStateNameForAbbreviation($prevState) ?></a>
+            </td>
+        </tr>
+
+<?		$lastStateAccumulated = $prevState;
 	}
 		
 	if (($yearArray != null) && ($countyToAccumulate != $county))
-	{
-?>
+	{ ?>
+
 		<tr><td class=firstcell>
 			<a href="./countyspecies.php?state=<?= $prevState ?>&county=<?= urlencode($countyToAccumulate) ?>"/>
 			<?= $countyToAccumulate ?> County
 			</a>
 			</td>
 
-<?
-	for ($year = 1996; $year <= 2004; $year++)
-	{
-?>
- 
+<?	for ($year = 1996; $year <= 2004; $year++)
+	{ ?>
         <td class=bordered align=right>
             <a href="./specieslist.php?county=<?= urlEncode($countyToAccumulate) ?>&year=<?= $year ?>">
 				&nbsp; <?= $yearArray[$year] ?>
             <a/>
         </td>
-<?
-	}
-?>
+<?	}?>
+
 		</tr>
-<?
-		$yearArray = null;
+
+<?		$yearArray = null;
 	}
 
 	$prevState = $state;	
