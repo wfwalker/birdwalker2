86dcf6146f837b6ed29fb52408569e7ac9b282e2
walker <walker>
2004-10-15 01:11:53 +0000

    refactored most of query string

--- yeardetail.php
+++ yeardetail.php
@@ -1,13 +1,14 @@
 
-<?php
+<?
 
 require("./birdwalker.php");
 
 $theYear = $_GET["year"];
 
-$yearCount = performCount("select count(distinct species.objectid) from species, sighting where sighting.Exclude!='1' and species.Abbreviation=sighting.SpeciesAbbreviation and year(sighting.TripDate)='" . $theYear . "'");
-
-?>
+$yearCount = performCount("
+    SELECT COUNT(DISTINCT species.objectid) FROM species, sighting
+      WHERE sighting.Exclude!='1' AND species.Abbreviation=sighting.SpeciesAbbreviation
+      AND year(sighting.TripDate)='" . $theYear . "'"); ?>
 
 <html>
   <head>
@@ -16,7 +17,7 @@ $yearCount = performCount("select count(distinct species.objectid) from species,
   </head>
   <body>
 
-<?php
+<?
 globalMenu();
 browseButtons("./yeardetail.php?year=", $theYear, 1996, $theYear - 1, $theYear + 1, 2004);
 navTrailBirds();
@@ -24,32 +25,28 @@ navTrailBirds();
 
     <div class=contentright>
       <div class="titleblock">	  
-<?      rightThumbnail("SELECT sighting.*, rand() AS shuffle FROM sighting WHERE sighting.Photo='1' AND Year(TripDate)='" . $theYear . "' ORDER BY shuffle LIMIT 1"); ?>
+<?      rightThumbnail("
+            SELECT sighting.*, rand() AS shuffle
+                FROM sighting
+                WHERE sighting.Photo='1' AND Year(TripDate)='" . $theYear . "'
+                ORDER BY shuffle LIMIT 1"); ?>
         <div class=pagetitle><?= $theYear ?> List</div>
     </div>
 
 
     <div class=heading><?= $yearCount ?> species</div>
 
-<?
-
-$gridQueryString = "
-    SELECT DISTINCT(CommonName), species.objectid AS speciesid, BIT_OR(1 << Month(TripDate)) AS mask
-      FROM sighting, species
-      WHERE sighting.Exclude='0' AND sighting.SpeciesAbbreviation=species.Abbreviation
-      AND year(sighting.TripDate)='" . $theYear . "'
-      GROUP BY sighting.SpeciesAbbreviation ORDER BY speciesid";
-
-$monthlyTotal = performQuery("
+<? $monthlyTotal = performQuery("
     SELECT COUNT(DISTINCT sighting.SpeciesAbbreviation) AS count, month(sighting.TripDate) AS month
       FROM sighting, species
       WHERE sighting.SpeciesAbbreviation=species.Abbreviation
       AND Year(sighting.TripDate)=" . $theYear . "
       GROUP BY month");
 
-formatSpeciesByMonthTable($gridQueryString, "&year=" . $theYear, $monthlyTotal); ?>
-
-</table>
+   formatSpeciesByMonthTable("
+     WHERE sighting.Exclude='0' AND sighting.SpeciesAbbreviation=species.Abbreviation AND sighting.LocationName=location.Name
+       AND year(sighting.TripDate)='" . $theYear . "'",
+      "&year=" . $theYear, $monthlyTotal); ?>
 
     </div>
   </body>
