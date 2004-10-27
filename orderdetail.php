f85b18aabe51562835f1376bc782eb85076ca69a
walker <walker>
2004-10-27 16:13:07 +0000

    use twoColumnSpeciesTable

--- orderdetail.php
+++ orderdetail.php
@@ -34,33 +34,14 @@ navTrailBirds($items);
       <div class=pagesubtitle> <?= $orderInfo["LatinName"] ?></div>
 	  <div class="titleblock">
         <div class=pagetitle><?= $orderInfo["CommonName"] ?></div>
-        <div class=metadata> <?= $orderCount ?> species</div>
       </div>
 
+
+     <div class=heading> <?= $orderCount ?> species</div>
 		
 <?php
 
-$divideByFamilys = ($orderCount > 20);
-$prevFamilyNum = -1;
-
-while($info = mysql_fetch_array($orderQuery))
-{
-	$familyNum =  floor($info["objectid"] / pow(10, 7));
-	
-	if ($divideByFamilys && ($prevFamilyNum != $familyNum))
-    {
-		$familyInfo = getFamilyInfo($familyNum * pow(10, 7));
-?>
-        <div class="heading"><i><?= $familyInfo["LatinName"] ?></i>, <?= $familyInfo["CommonName"] ?></div>
-<?
-    }
-?>	
-
-	<div class=firstcell><a href="./speciesdetail.php?id=<?= $info["objectid"] ?>"><?= $info["CommonName"] ?></a></div>
-
-<?
-	$prevFamilyNum = $familyNum;
-}
+	formatTwoColumnSpeciesList($orderQuery);
 ?>
 
     </div>
