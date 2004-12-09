ae30441c7d310c105bdaf2a66686d06e52757bdc
walker <walker>
2004-12-09 02:38:42 +0000

    compress all four species details php's into one using new query objects

--- speciesdetail.php
+++ speciesdetail.php
@@ -2,27 +2,25 @@
 <?php
 
 require("./birdwalker.php");
+require("./sightingquery.php");
+require("./locationquery.php");
+require("./tripquery.php");
+
+$view = param($_GET, "view", "lists");
 
 $speciesID = param($_GET, 'id', 22330150100);
 $speciesInfo = getSpeciesInfo($speciesID);
 
-$speciesTripQuery = performQuery("
-    SELECT sighting.Notes as sightingNotes, trip.*, date_format(Date, '%b %e, %Y') as niceDate
-      FROM trip, sighting
-      WHERE '" . $speciesInfo["Abbreviation"] . "'=sighting.SpeciesAbbreviation
-      AND sighting.TripDate=trip.Date
-      ORDER BY trip.Date desc");
-$speciesTripCount = mysql_num_rows($speciesTripQuery);
+if ($view != "photo")
+{
+$tripQuery = new TripQuery;
+$tripQuery->setSpeciesID($speciesID);
+$tripCount = $tripQuery->getTripCount();
 
-$speciesLocationListQuery = performQuery( "
-    SELECT distinct(location.objectid), location.*
-      FROM location, sighting
-      WHERE sighting.SpeciesAbbreviation='" . $speciesInfo["Abbreviation"] . "'
-      AND sighting.LocationName=location.Name
-      ORDER BY State, County, Name");
-
-$speciesLocationCount = mysql_num_rows($speciesLocationListQuery);
-
+$locationQuery = new LocationQuery;
+$locationQuery->setSpeciesID($speciesID);
+$locationCount = $locationQuery->getLocationCount();
+}
 ?>
 
 <html>
@@ -36,17 +34,14 @@ $speciesLocationCount = mysql_num_rows($speciesLocationListQuery);
 
 <?php
 globalMenu();
-speciesBrowseButtons($speciesID, "");
+speciesBrowseButtons($speciesID, $view);
 navTrailSpecies($speciesID);
 ?>
 
   <div class=contentright>
     <div class="pagesubtitle"><?= $speciesInfo["LatinName"] ?></div>
 	<div class="titleblock">
-<? rightThumbnail("
-    SELECT * FROM sighting
-      WHERE SpeciesAbbreviation='" . $speciesInfo["Abbreviation"] . "' and Photo='1' ORDER BY TripDate DESC LIMIT 1"); ?>
-
+<?    if ($view != "photo") { rightThumbnailSpecies($speciesInfo["Abbreviation"]); } ?>
       <div class="pagetitle"><?= $speciesInfo["CommonName"] ?></div>
       <div class=metadata>
 <?  if (strlen($speciesInfo["ReferenceURL"]) > 0) { ?>
@@ -54,20 +49,35 @@ navTrailSpecies($speciesID);
 <?  }
     if ($speciesInfo["ABACountable"] == '0') { ?>
         <div>NOT ABA COUNTABLE</div>
-<?  }
-    speciesViewLinks($speciesID); ?>
+<?  } ?>
+      <a href="./speciesdetail.php?id=<?=$speciesID?>">list</a> |
+      <a href="./speciesdetail.php?view=bymonth&id=<?=$speciesID?>">by month</a> |
+      <a href="./speciesdetail.php?view=byyear&id=<?=$speciesID?>">by year</a> |
+      <a href="./speciesdetail.php?view=photo&id=<?=$speciesID?>">photo</a>
 
-   </div>
+      </div>
    </div>
 
    <div class=sighting-notes><?= $speciesInfo["Notes"] ?></div>
-       <div class=heading><?= $speciesTripCount ?> trip<? if ($speciesTripCount > 1) echo 's' ?></div>
 
-<?     formatTwoColumnTripList($speciesTripQuery) ?>
 
-       <div class=heading><?= $speciesLocationCount ?> location<? if ($speciesLocationCount > 1) echo 's' ?></div>
-
-<?     formatTwoColumnLocationList($speciesLocationListQuery) ?>
+<? if ($view == "lists") { ?>
+       <div class=heading><?= $tripCount ?> trip<? if ($tripCount > 1) echo 's' ?></div>
+<?     $tripQuery->formatTwoColumnTripList() ?>
+       <div class=heading><?= $locationCount ?> location<? if ($locationCount > 1) echo 's' ?></div>
+<?     $locationQuery->formatTwoColumnLocationList() ?>
+<? } elseif ($view == "bymonth") { ?>
+       <div class=heading><?= $tripCount ?> trips, <?= $locationCount ?> locations</div>
+<?     $locationQuery->formatLocationByMonthTable(); ?>
+<? } elseif ($view == "byyear") { ?>
+       <div class=heading><?= $tripCount ?> trips, <?= $locationCount ?> locations</div>
+<?     $locationQuery->formatLocationByYearTable(); ?>
+<? } elseif ($view == "photo") {
+	   $sightingQuery = new SightingQuery;
+       $sightingQuery->setSpeciesID($speciesID); ?>
+       <div class=heading><?= $sightingQuery->getPhotoCount(); ?> photos</div>
+<?     $sightingQuery->formatPhotos(); ?>
+<? } ?>
 
    </div>
 </body>
