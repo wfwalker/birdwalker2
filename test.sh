75007bb00f6f202b2e7ae57f479cab42f220f080
walker <walker>
2004-12-09 02:36:32 +0000

    don't time things

--- test.sh
+++ test.sh
@@ -6,7 +6,7 @@ echo -n "" > testresults.txt
 
 for file in $(ls *.php); do
 	log "---- $file ----"
-	time php ./$file | egrep "(mysql|Fatal error)" >> testresults.txt
+	php ./$file | egrep "(mysql|Fatal error)" >> testresults.txt
 	ec=$?
 	if [ $ec -eq 1 ]; then
 		log "---- $file PASSED ----"
