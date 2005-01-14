6c5f4b461d6a6f2e9ed0051f8aa441b5cfb191c8
walker <walker>
2005-01-14 01:33:14 +0000

    use getLatestYear fixups to use new API's

--- onthisdate.php
+++ onthisdate.php
@@ -12,7 +12,7 @@ $tripsOnThisDate = performQuery("
       FROM trip
       WHERE Month(Date)='" . ($localtimearray["tm_mon"] + 1) . "' AND
         DayOfYear(Date)>='" . $dayStart . "' AND DayOfYear(Date)<='" . $dayStop . "'
-        AND Year(Date)<2004
+        AND Year(Date)<" . getLatestYear() . "
       ORDER BY Date DESC"); ?>
 
 <html>
@@ -36,14 +36,20 @@ disabledBrowseButtons();
       </div>
 
 <?    while ($info = mysql_fetch_array($tripsOnThisDate))
-      { ?>
+      {
+          $tripSpeciesCount = performCount("SELECT COUNT(DISTINCT(sighting.objectid)) from sighting where sighting.TripDate='" . $info["Date"] . "'"); ?>
 		  <p>&nbsp;</p>
 
           <div class="pagesubtitle"><?= $info["niceDate"] ?></div>
 
 		  <div class="titleblock">
-<?         rightThumbnail("SELECT * FROM sighting WHERE Photo='1' AND TripDate='" . $info["Date"] . "' LIMIT 1"); ?>
-              <span class="pagetitle"><a href="./tripdetail.php?id=<?=$info["objectid"]?>"/><?= $info["Name"] ?></a></span>
+              <span class="pagetitle">
+                  <a href="./tripdetail.php?id=<?=$info["objectid"]?>"/>
+<?                    rightThumbnail("SELECT * FROM sighting WHERE Photo='1' AND TripDate='" . $info["Date"] . "' LIMIT 1", false); ?>
+                      <?= $info["Name"] ?>
+                  </a>
+              </span>
+              <div class="subheading"><?= $tripSpeciesCount ?> species</div>
           </div>
 
           <div class=report-content><?= $info["Notes"] ?><br clear="all"/></div>
