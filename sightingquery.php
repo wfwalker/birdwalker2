d261c847320b0439f10bb9d4a8b6ef9c8ea3e9d7
walker <walker>
2005-01-21 01:39:05 +0000

    query for sightings. this should have been in the CVS ages ago!

---
+++ sightingquery.php
@@ -0,0 +1,243 @@
+
+<?php
+
+class SightingQuery
+{
+	// constrain this query to a particular location
+	var $mLocationID;
+	// constrain this query to a particular county
+	var $mCounty;
+	// constrain this query to a particular state
+	var $mState;
+
+	// constrain this query to a trip
+	var $mTripID;
+	// constrain this query to a particular month
+	var $mMonth;
+	// constrain this query to a particular year
+	var $mYear;
+
+	// constrain this query to a particular species	var $mSpeciesID;
+	// constrain this query to a particular family
+	var $mFamily;
+	// constrain this query to a particular order
+	var $mOrder;
+
+	function SightingQuery()
+	{
+		$this->setLocationID("");
+		$this->setCounty("");
+		$this->setState("");
+
+		$this->setTripID("");
+		$this->setMonth("");
+		$this->setYear("");
+
+		$this->setFamily("");
+		$this->setOrder("");
+	}
+
+	function setLocationID($inValue) { $this->mLocationID = $inValue; }
+	function setCounty($inValue) { $this->mCounty = $inValue; }
+	function setState($inValue) { $this->mState = $inValue; }
+
+	function setTripID($inValue) { $this->mTripID = $inValue; }
+	function setMonth($inValue) { $this->mMonth = $inValue; }
+	function setYear($inValue) { $this->mYear = $inValue; }
+
+    function setSpeciesID($inValue) { $this->mSpeciesID = $inValue; }
+	function setFamily($inValue) { $this->mFamily = $inValue; }
+	function setOrder($inValue) { $this->mOrder = $inValue; }
+
+	function getSelectClause()
+	{
+		return "SELECT DISTINCT sighting.objectid,
+            sighting.*,
+            trip.objectid as tripid, date_format(sighting.TripDate, '%M %e, %Y') AS niceDate, trip.*,
+            location.objectid as locationid, location.*,
+            species.objectid as speciesid, species.*";
+	}
+
+	function getFromClause()
+	{
+		return " FROM sighting, species, location, trip";
+	}
+
+
+	function getWhereClause()
+	{
+		$whereClause = "WHERE
+            location.Name=sighting.LocationName AND
+            species.Abbreviation=sighting.SpeciesAbbreviation AND
+            trip.Date=sighting.TripDate ";
+
+		if ($this->mTripID != "") {
+			$tripInfo = getTripInfo($this->mTripID);
+			$whereClause = $whereClause . " AND sighting.TripDate='" . $tripInfo["Date"] . "'";
+		}
+
+		if ($this->mLocationID != "") {
+			$whereClause = $whereClause . " AND location.objectid=" . $this->mLocationID;
+		} elseif ($this->mCounty != "") {
+			$whereClause = $whereClause . " AND location.County='" . $this->mCounty . "'";
+		} elseif ($this->mState != "") {
+			$whereClause = $whereClause . " AND location.State='" . $this->mState . "'";
+		}
+
+		if ($this->mSpeciesID != "") {
+			$speciesInfo = getSpeciesInfo($this->mSpeciesID);
+			$whereClause = $whereClause . " AND
+              sighting.SpeciesAbbreviation='" . $speciesInfo["Abbreviation"] . "'"; 
+		} elseif ($this->mFamily != "") {
+			$whereClause = $whereClause . " AND
+              species.objectid >= " . $this->mFamily * pow(10, 7) . " AND
+              species.objectid < " . ($this->mFamily + 1) * pow(10, 7);
+		} elseif ($this->mOrder != "") {
+			$whereClause = $whereClause . " AND
+              species.objectid >= " . $this->mOrder * pow(10, 9) . " AND
+              species.objectid < " . ($this->mOrder + 1) * pow(10, 9);
+		}
+		
+		if ($this->mMonth !="") {
+			$whereClause = $whereClause . " AND Month(TripDate)=" . $this->mMonth;
+		}
+		if ($this->mYear !="") {
+			$whereClause = $whereClause . " AND Year(TripDate)=" . $this->mYear;
+		}
+
+		return $whereClause;
+	}
+
+	function getParams()
+	{
+		$params = "";
+
+		if ($this->mLocationID != "") {
+			$params = $params . "&locationid=" . $this->mLocationID;
+		} elseif ($this->mCounty != "") {
+			$params = $params . "&county=" . $this->mCounty . "'";
+		} elseif ($this->mState != "") {
+			$params = $params . "&state=" . $this->mState . "'";
+		}
+
+		if ($this->mSpeciesID != "") {
+			$params = $params . "&speciesID=" . $this->mSpeciesID;
+		} elseif ($this->mFamily != "") {
+			$params = $params . "&family=" . $this->mFamily;
+		} elseif ($this->mOrder != "") {
+			$params = $params . "&order=" . $this->mOrder;
+		}
+		
+		if ($this->mMonth !="") {
+			$params = $params . "&month=" . $this->mMonth;
+		}
+		if ($this->mYear !="") {
+			$params = $params . "&year=" . $this->mYear;
+		}
+
+		return $params;
+	}
+
+	function performQuery()
+	{
+		return performQuery(
+			$this->getSelectClause() . " " .
+			$this->getFromClause() . " " .
+			$this->getWhereClause() . " ORDER BY sighting.objectid");
+	}
+
+	function getPageTitle()
+	{
+		// todo add species, family, order
+		$pageTitle = "";
+
+		if ($this->mLocationID != "") {
+			$locationInfo = getLocationInfo($this->mLocationID); 
+			$pageTitle = $locationInfo["Name"];
+		} elseif ($this->mCounty != "") {
+			$pageTitle = $this->mCounty . " County";
+		} elseif ($this->mState != "") {
+			$pageTitle = getStateNameForAbbreviation($this->mState);
+		}
+
+		if ($this->mTripID != "") {
+			$tripInfo = getTripInfo($this->mTripID); 
+			$pageTitle = $tripInfo["Name"];
+		}
+		if ($this->mMonth !="") {
+			if ($pageTitle == "") $pageTitle = getMonthNameForNumber($this->mMonth);
+			else $pageTitle = $pageTitle . ", " . getMonthNameForNumber($this->mMonth);
+		}
+		if ($this->mYear !="") {
+			if ($pageTitle == "") $pageTitle = $this->mYear;
+			else $pageTitle = $pageTitle . ", " . $this->mYear;
+		}
+
+		// todo, need order and family in here
+
+
+		return $pageTitle; 
+	}
+
+	function rightThumbnail($anchorFlag)
+	{
+		rightThumbnail("
+          SELECT sighting.*, rand() AS shuffle " .
+			$this->getFromClause() . "  " .
+			$this->getWhereClause() . "
+            AND sighting.Photo='1'
+            ORDER BY shuffle LIMIT 1", $anchorFlag);
+	}
+
+	function formatTwoColumnSpeciesList()
+	{
+		formatTwoColumnSpeciesList($this);
+	}
+
+	function formatSpeciesByYearTable()
+	{
+		$annualTotal = performQuery("
+          SELECT COUNT(DISTINCT species.objectid) AS count, year(sighting.TripDate) AS year " .
+            $this->getFromClause() . " " .
+		    $this->getWhereClause() . "
+			GROUP BY year");
+
+		formatSpeciesByYearTable($this, $this->getParams(), $annualTotal);
+	}
+
+	function formatSpeciesByMonthTable()
+	{
+		$monthlyTotal = performQuery("
+          SELECT COUNT(DISTINCT species.objectid) AS count, month(sighting.TripDate) AS month " .
+            $this->getFromClause() . " " .
+		    $this->getWhereClause() . "
+			GROUP BY month");
+
+		formatSpeciesByMonthTable($this, $this->getParams(), $monthlyTotal);
+	}
+
+	function getPhotos()
+	{
+		return performQuery("
+          SELECT sighting.* " .
+			$this->getFromClause() . "  " .
+			$this->getWhereClause() . "
+            AND sighting.Photo='1'
+            ORDER BY sighting.TripDate DESC");
+	}
+
+	function getPhotoCount()
+	{
+		return performCount("
+          SELECT COUNT(DISTINCT sighting.objectid) " .
+			$this->getFromClause() . "  " .
+			$this->getWhereClause() . "
+            AND sighting.Photo='1'");
+	}
+
+	function formatPhotos()
+	{
+		formatPhotos($this);
+	}
+}
+?>
