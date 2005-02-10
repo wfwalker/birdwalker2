e8b839d5078483a550295e035c4637d5c8be5556
walker <walker>
2005-02-10 18:23:34 +0000

    whoops fix regexp so as not to omit credits.php

--- upload-php.sh
+++ upload-php.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
-filesToCopy=$(find . -type f -name \*.php -not -name \*edit\* -and -not -name speciesindex2.php -and -not -name photosneeded.php -and -not -name targetyearbirds.php -and -not -name \*create\*)
+filesToCopy=$(find . -type f -name \*.php -not -name \*edit\.\* -and -not -name speciesindex2.php -and -not -name photosneeded.php -and -not -name targetyearbirds.php -and -not -name \*create\*)
 
 scp stylesheet.css $filesToCopy walker@sven.spflrc.org:~/.www/
