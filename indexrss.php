f52a7d932d3cc519e520e03dcc2e7e6bafb4efc1
walker <walker>
2005-02-17 04:12:42 +0000

    make it easier to change how many trips

--- indexrss.php
+++ indexrss.php
@@ -3,7 +3,8 @@
 <?
 require("./birdwalker.php");
 // Sat, 07 Sep 2002 00:00:01 GMT
-$latestTrips = performQuery("select *, date_format(Date, '%a, %d %b %Y %T GMT') AS niceDate from trip order by Date desc limit 5");
+$numberOfTrips = 10;
+$latestTrips = performQuery("select *, date_format(Date, '%a, %d %b %Y %T GMT') AS niceDate from trip order by Date desc limit " . $numberOfTrips);
 ?>
 
 <rss version="2.0">
@@ -15,7 +16,7 @@ $latestTrips = performQuery("select *, date_format(Date, '%a, %d %b %Y %T GMT') 
     <language>en-US</language>
     <generator>birdWalker</generator>
 
-<?    for ($index = 0; $index < 5; $index++)
+<?    for ($index = 0; $index < $numberOfTrips; $index++)
 	  {
 		  $info = mysql_fetch_array($latestTrips); ?>
 
