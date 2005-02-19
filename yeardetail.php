483a9fd3ead12a6ba74a1722c96904980f011d57
walker <walker>
2005-02-19 02:48:48 +0000

    rehabilitate yeardetail, woo!

--- yeardetail.php
+++ yeardetail.php
@@ -3,22 +3,27 @@
 
 require_once("./birdwalker.php");
 require_once("./speciesquery.php");
+require_once("./sightingquery.php");
+require_once("./locationquery.php");
+require_once("./map.php");
 
-$theYear = param($_GET, "year", "1998");
+$year = param($_GET, "year", "1998");
+$view = param($_GET, "view", "species");
+
 $speciesQuery = new SpeciesQuery;
-$speciesQuery->setYear($theYear);
+$speciesQuery->setFromRequest($_GET);
 
 ?>
 
 <html>
 
-  <? htmlHead($theYear . " Report") ?>
+  <? htmlHead($year) ?>
 
   <body>
 
 <?
 globalMenu();
-browseButtons("./yeardetail.php?year=", $theYear, getEarliestYear(), $theYear - 1, $theYear + 1, getLatestYear());
+browseButtons("./yeardetail.php?view=" . $view . "&year=", $year, getEarliestYear(), $year - 1, $year + 1, getLatestYear());
 navTrailBirds();
 ?>
 
@@ -27,15 +32,80 @@ navTrailBirds();
 <?      rightThumbnail("
             SELECT sighting.*, rand() AS shuffle
                 FROM sighting
-                WHERE sighting.Photo='1' AND Year(TripDate)='" . $theYear . "'
+                WHERE sighting.Photo='1' AND Year(TripDate)='" . $year . "'
                 ORDER BY shuffle LIMIT 1", true); ?>
-        <div class=pagetitle><?= $theYear ?> List</div>
-    </div>
+        <div class=pagetitle><?= $year ?></div>
+          <div class=metadata>
+            locations:
+              <a href="./yeardetail.php?view=locations&year=<?= $year ?>">list</a> |
+	          <a href="./yeardetail.php?view=locationsbymonth&year=<?= $year ?>">by month</a> |
+              <a href="./yeardetail.php?view=map&year=<?= $year ?>">map</a> <br/>
+            species:	
+              <a href="./yeardetail.php?view=species&year=<?= $year ?>">list</a> |
+              <a href="./chronocayearlist.php?year=<?= $year ?>">ABA</a> |
+	          <a href="./yeardetail.php?view=speciesbymonth&year=<?= $year ?>">by month</a> |
+              <a href="./yeardetail.php?view=species&view=photo&year=<?= $year ?>">photo</a><br/>
+          </div>
+		</div>
 
+<?
+if ($view == 'species')
+{
+	$speciesQuery = new SpeciesQuery;
+	$speciesQuery->setFromRequest($_GET);
+	countHeading( $speciesQuery->getSpeciesCount(), "species");
+	$speciesQuery->formatTwoColumnSpeciesList(); 
+}
+elseif ($view == 'speciesbyyear')
+{
+	$speciesQuery = new SpeciesQuery;
+	$speciesQuery->setFromRequest($_GET);
+	countHeading( $speciesQuery->getSpeciesCount(), "species");
+	$speciesQuery->formatSpeciesByYearTable(); 
+}
+elseif ($view == 'speciesbymonth')
+{
+	$speciesQuery = new SpeciesQuery;
+	$speciesQuery->setFromRequest($_GET);
+	countHeading( $speciesQuery->getSpeciesCount(), "species");
+	$speciesQuery->formatSpeciesByMonthTable(); 
+}
+elseif ($view == 'locations')
+{
+    $locationQuery = new LocationQuery;
+	$locationQuery->setFromRequest($_GET);
+	countHeading( $locationQuery->getLocationCount(), "location");
+	$locationQuery->formatTwoColumnLocationList();
+}
+elseif ($view == 'locationsbyyear')
+{
+    $locationQuery = new LocationQuery;
+	$locationQuery->setFromRequest($_GET);
+	countHeading( $locationQuery->getLocationCount(), "location");
+	$locationQuery->formatLocationByYearTable();
+}
+elseif ($view == 'locationsbymonth')
+{
+    $locationQuery = new LocationQuery;
+	$locationQuery->setFromRequest($_GET);
+	countHeading( $locationQuery->getLocationCount(), "location");
+	$locationQuery->formatLocationByMonthTable();
+}
+else if ($view == "map")
+{
+	countHeading($locationCount, "location");
+	$map = new Map("./countydetail.php");
+	$map->setFromRequest($_GET);
+	$map->draw();
+}
+elseif ($view == 'photo')
+{
+	$sightingQuery = new SightingQuery;
+	$sightingQuery->setFromRequest($_GET);
+	$sightingQuery->formatPhotos();
+}
 
-		<div class=heading><?= $speciesQuery->getSpeciesCount() ?> species</div>
-
-<? $speciesQuery->formatSpeciesByMonthTable(); ?>
+?>
 
     </div>
   </body>
