da7558b27b5647b4942b7dd1c8e8605698ce420c
walker <walker>
2005-03-04 01:25:29 +0000

    add missing family to setFromRequest

--- birdwalkerquery.php
+++ birdwalkerquery.php
@@ -53,6 +53,7 @@ class BirdWalkerQuery
 	{
 		$this->setTripID(param($_GET, "tripid", ""));
 		$this->setSpeciesID(param($_GET, "speciesid", ""));
+		$this->setFamily(param($_GET, "family", ""));
 		$this->setLocationID(param($_GET, "locationid", ""));
 		$this->setYear(param($_GET, "year", ""));
 		$this->setMonth(param($_GET, "month", ""));
