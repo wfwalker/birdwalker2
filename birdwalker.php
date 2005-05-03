91a4e772a126520f570a0357f946917aee123396
walker <walker>
2005-05-03 19:40:50 +0000

    add metadata link alternate for RSS

--- birdwalker.php
+++ birdwalker.php
@@ -38,6 +38,7 @@ echo "<!DOCTYPE  HTML PUBLIC  \"-//W3C//DTD HTML 4.01 Transitional//EN\">";
 ?>
 <html>
   <head>
+    <link rel="alternate" type="application/atom+xml" title="Atom" href="./indexrss.php" />
     <link rel="SHORTCUT ICON" href="./images/favicon.ico">
     <link title="Style" href="./stylesheet.css" type="text/css" rel="stylesheet">
       <title>birdWalker | <?= $title ?></title>
