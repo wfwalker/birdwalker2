7ca38ec4aab2eceb4023d2d5bc2c584a547fde38
walker <walker>
2005-08-10 07:32:38 +0000

    granny's bday

--- trip.sql
+++ trip.sql
@@ -420,3 +420,4 @@ INSERT INTO trip VALUES (420,'Bob Power','','Kern River Day Four','An awesome dr
 INSERT INTO trip VALUES (421,'Bill','','Morning Photo Safari','I made one last trip with the 500mm looking for nesting passerines. I had only a few good shots, but it was a very nice walk.','2005-06-28');
 INSERT INTO trip VALUES (422,'Mary and Bill','','Stroll after work','So, there we were fresh from four days of birding in the mountains, three days left on the 500mm f/4L rental. We had to go back out in the field!','2005-06-27');
 INSERT INTO trip VALUES (423,'Mary and Bill','','Stroll after work','We went to check out reports of Phalaropes on the pond, which we found. We tried to make some of the Curlews into Whimbrels, but did not succeed.','2005-07-19');
+INSERT INTO trip VALUES (424,'Mary and Bill','','Grandma\'s Birthday','','2005-08-08');
