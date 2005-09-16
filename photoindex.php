c2234f60c287a2ecb64962c8c29ffdf73c1dc185
walker <walker>
2005-09-16 15:39:18 +0000

    use SightingQuery->formatPhotos()

--- photoindex.php
+++ photoindex.php
@@ -2,6 +2,7 @@
 <?php
 
 require_once("./birdwalker.php");
+require_once("./request.php");
 
 $photoCount = performCount("select count(*) from sighting where Photo='1'");
 $photoSpeciesCount = performCount("select count(distinct(sighting.SpeciesAbbreviation)) from sighting where Photo='1'");
@@ -13,56 +14,26 @@ navTrail();
 ?>
 
 
-    <div class=contentright>
-	  <div class="pagesubtitle">Index</div>
-      <div class="titleblock">	  
-	    <div class=pagetitle>Photos</div>
-        <div class=metadata>
-          <a href="./photoindextaxo.php">by species</a> |
-          <a href="./photoindex.php">by date</a> |
-          <a href="./photoindexlocation.php">by location</a>
-        </div>
+  <div class=contentright>
+	<div class="pagesubtitle">Index</div>
+
+    <div class="titleblock">	  
+	  <div class="pagetitle">Photos</div>
+      <div class="viewlinks">
+        <a href="./photoindextaxo.php">by species</a> |
+        <a href="./photoindex.php">by date</a> |
+        <a href="./photoindexlocation.php">by location</a>
       </div>
+    </div>
 
-<div class=heading><?= $photoCount . " photos covering " . $photoSpeciesCount . " species"; ?></div>
-
-<table cellpadding=4>
-
-<?php
-$photoQuery = performQuery("
-  SELECT CONCAT(sighting.TripDate, sighting.objectid) AS photoOrder, sighting.*, sighting.objectid as sightingid,
-      species.CommonName, DATE_FORMAT(sighting.TripDate, '%M %e, %Y') AS niceDate
-    FROM sighting, species WHERE Photo='1' AND sighting.SpeciesAbbreviation=species.Abbreviation
-    ORDER BY photoOrder DESC");
-
-
-$counter = 0;
-
-while($info = mysql_fetch_array($photoQuery))
-{
-	if (($counter % 2) == 0)
-	{
-?>      <tr> <?
-    }
-?>
-    <td valign=top align=right><?= getThumbForSightingInfo($info) ?></td>
-    <td class=report-content valign=top>
-	    <?= $info["CommonName"] ?><br/>
-        <?= $info["niceDate"] ?><br/>
-        <?= $info["LocationName"] ?></td>
-<?
-    if (($counter % 4) == 1)
-	{
-?>      </tr> <?
-    }
-
-    $counter++;
-}
-?>
-
-</table>
+  <div class=heading><?= $photoCount . " photos covering " . $photoSpeciesCount . " species"; ?></div>
 
 <?
+
+$request = new Request;
+$sightingQuery = new SightingQuery($request);
+$sightingQuery->formatPhotos(); ?>
+
 footer();
 ?>
 
