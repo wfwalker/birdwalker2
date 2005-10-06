374b2fc649e075520b08c40b9d8949d977b98786
walker <walker>
2005-10-06 17:40:20 +0000

    new javascrip-based opening of slideshow window new context-sensitive globalmenu with css-based dink triangles

--- request.php
+++ request.php
@@ -198,6 +198,18 @@ class Request
 			" speciesid " . $this->mSpeciesID . " family " . $this->mFamilyID . " order " . $this->mOrderID . " -->\n\n";
 	}
 
+	function command($linkText)
+	{
+		if (strlen($linkText) > 18)
+		{
+			return $this->linktoSelf(strtolower(substr($linkText, 0, 18) . "..."), "commandlink");
+		}
+		else
+		{
+			return $this->linkToSelf(strtolower($linkText), "commandlink");
+		}
+	}
+
 	function linkToSelf($linkText, $class = "")
 	{
 		if ($class == "")
@@ -219,8 +231,34 @@ class Request
 		$newRequest->setLatitude("");
 		$newRequest->setLongitude("");
 		$newRequest->setScale("");
+?>
+<SCRIPT LANGUAGE="JavaScript">
+<!--
 
-		return "<a target=\"slideshow\" href=\"./slideshow.php?" . $this->getParams() . "\">slideshow</a>";
+// Copyright (c) 2000 internet.com Corp.
+// http://www.webreference.com/js/
+// License is granted if and only if this entire
+// copyright notice is included. By Tomer Shiran.
+
+function launch(newURL, newName, newFeatures, orgName) {
+  var remote = open(newURL, newName, newFeatures);
+  if (remote.opener == null)
+    remote.opener = window;
+  remote.opener.name = orgName;
+  return remote;
+}
+
+function launchSlideshow() {
+  myRemote = launch("./slideshow.php?<?= $this->getParams() ?>",
+                    "myRemote",
+                    "height=600,width=650,alwaysLowered=0,alwaysRaised=0,channelmode=0,dependent=0,directories=0,fullscreen=0,hotkeys=1,location=0,menubar=0,resizable=0,scrollbars=0,status=0,titlebar=1,toolbar=0,z-lock=0",
+                    "myWindow");
+}
+
+// -->
+</SCRIPT>
+<a  href="javascript:void(0)" onclick="launchSlideshow()">slideshow</a>
+<?
 	}
 
 	function linkToSelfChangeView($view, $linkText)
@@ -306,7 +344,7 @@ class Request
 			$orderRequest->setSpeciesID("");
 			$orderRequest->setFamilyID("");
 			
-			$items[] = $orderRequest->linkToSelf(strtolower($orderInfo["LatinName"]));
+			$items[] = $orderRequest->command($orderInfo["LatinName"]);
 		}
 		
 		if ($this->getSpeciesID() != "")
@@ -317,7 +355,7 @@ class Request
 			$familyRequest->setPageScript("familydetail.php");
 			$familyRequest->setSpeciesID("");
 			
-			$items[] = $familyRequest->linkToSelf(strtolower($familyInfo["LatinName"]));
+			$items[] = $familyRequest->command($familyInfo["LatinName"]);
 		}
 		
 		navTrail($items);
@@ -336,7 +374,7 @@ class Request
 			$stateRequest->setLocationID("");
 			$stateRequest->setCounty("");
 			
-			$items[] = $stateRequest->linkToSelf(strtolower($stateInfo["Name"]));
+			$items[] = $stateRequest->command($stateInfo["Name"]);
 		}
 		
 		if ($this->getLocationID() != "")
@@ -345,7 +383,7 @@ class Request
 			$countyRequest->setPageScript("countydetail.php");
 			$countyRequest->setLocationID("");
 			
-			$items[] = $countyRequest->linkToSelf(strtolower($this->getCounty() . " county"));
+			$items[] = $countyRequest->command($this->getCounty() . " county");
 		}
 		
 		navTrail($items);
@@ -363,7 +401,7 @@ class Request
 			$yearRequest->setMonth("");
 			$yearRequest->setView("trips");
 			
-			$items[] = $yearRequest->linkToSelf($this->getYear());
+			$items[] = $yearRequest->command($this->getYear());
 		}
 		
 		if ($this->getTripID() != "")
@@ -373,20 +411,18 @@ class Request
 			$monthRequest->setTripID("");
 			$monthRequest->setView("tripsummaries");
 			
-			$items[] = $monthRequest->linkToSelf(strtolower(getMonthNameForNumber($this->getMonth())));
+			$items[] = $monthRequest->command(getMonthNameForNumber($this->getMonth()));
 		}
 		
 		navTrail($items);
 	}
 
-	function handleStandardViews($inDefaultView)
+	function handleStandardViews()
 	{
-		if ($this->getView() == "") { $this->setView($inDefaultView); } 
-
 		if ($this->getView() == 'trips')
 		{
 			$tripQuery = new TripQuery($this);
-			countHeading( $tripQuery->getTripCount(), "trip");
+			doubleCountHeading($tripQuery->getTripCount(), "trip", $tripQuery->getPhotoCount(), "with photo");
 			$tripQuery->formatTwoColumnTripList();
 		}
 		elseif ($this->getView() == 'tripsummaries')
@@ -398,7 +434,7 @@ class Request
 		elseif ($this->getView() == 'species')
 		{
 			$speciesQuery = new SpeciesQuery($this);
-			countHeading( $speciesQuery->getSpeciesCount(), "species");
+			doubleCountHeading($speciesQuery->getSpeciesCount(), "species", $speciesQuery->getPhotoCount(), "with photo");
 			$speciesQuery->formatTwoColumnSpeciesList(); 
 		}
 		elseif ($this->getView() == 'speciesbyyear')
@@ -415,9 +451,8 @@ class Request
 		}
 		elseif ($this->getView() == 'locations')
 		{
-			$this->setView("species"); // TODO: need to switch to species view here
 			$locationQuery = new LocationQuery($this);
-			countHeading( $locationQuery->getLocationCount(), "location");
+			doubleCountHeading( $locationQuery->getLocationCount(), "location", $locationQuery->getPhotoCount(), "with photo");
 			$locationQuery->formatTwoColumnLocationList($this->getView(), false);
 		}
 		elseif ($this->getView() == 'locationsbyyear')
@@ -453,52 +488,261 @@ class Request
 		}
 	}
 
-	function viewLinks()
+
+	function optionSelectedViewHelper($inName, $inValue)
+	{
+		echo "<option value=\"" . $inValue . "\"";
+		if ($this->getView() == $inValue) echo " selected";
+		echo ">" . $inName . "</option>";
+	}
+	
+	function viewLinks($inDefaultView)
 	{ 
-		?><div class="viewlinks"><?
+		if ($this->getView() == "") { $this->setView($inDefaultView); } 
+
+		$tempRequest = new Request;
+		$tempRequest->setView("");	 
+?>
+
+
+<SCRIPT LANGUAGE="JavaScript">
+<!--
+function changeView()
+{
+	box = document.forms[0].viewChooser;
+	destination = "./<?= $tempRequest->getPageScript() ?>?<?= $tempRequest->getParams() ?>&view=" + box.options[box.selectedIndex].value;
+	if (destination) location.href = destination;
+}
+// -->
+</SCRIPT>
+
+	    <div class="viewlinks">
+	      <form class="viewlinks">
+	        view: <select name="viewChooser" onChange="changeView()"> <?
 
 		if ($this->getTripID() == "")
-		{
-			echo "trips: ";
-			echo $this->linkToSelfChangeView("trips", "list") . " | ";			
-			echo $this->linkToSelfChangeView("tripsummaries", "summaries") . "<br/>";			
+		{ ?>
+		  <optgroup label="trips"><?
+		    $this->optionSelectedViewHelper("trip list", "trips");
+		    $this->optionSelectedViewHelper("trip summaries", "tripsummaries");
 		}
 
 		if ($this->getLocationID() == "")
-		{
-			echo "locations: "; 
-			echo $this->linkToSelfChangeView("locations", "list") . " | ";
+		{ ?>
+			<optgroup label="locations"><?
+			
+			if ($this->getTripID() == "" )
+			{
+				$this->optionSelectedViewHelper("location list", "locations");
+			}
+
 			if ($this->getMonth() == "" )
 			{
-				echo $this->linkToSelfChangeView("locationsbymonth", "by month") . " | ";
+				$this->optionSelectedViewHelper("locations by month", "locationsbymonth");
+
 				if ($this->getYear() == "" )
 				{
-					echo $this->linkToSelfChangeView("locationsbyyear", "by year") . " | ";
+					$this->optionSelectedViewHelper("locations by year", "locationsbyyear");
 				}
 			}
-			echo $this->linkToSelfChangeView("map", "map");
-			echo "<br/>";
+
+			$this->optionSelectedViewHelper("map", "map"); ?>
+			</optgroup> <?
 		}
 		
 		if ($this->getSpeciesID() == "")
-		{
-			echo "species: ";
-			echo $this->linkToSelfChangeView("species", "list") . " | "; 
-			echo $this->linkToSelfChangeView("chrono", "ABA") . " | "; 
+		{ ?>
+			<optgroup label="species"> <?
+		    $this->optionSelectedViewHelper("species list", "species");
+		    if ($this->getTripID() == "" )
+			{
+				$this->optionSelectedViewHelper("ABA", "chrono");
+			}
+
 			if ($this->getMonth() == "" )
 			{
-				echo $this->linkToSelfChangeView("speciesbymonth", "by month"); 
+				$this->optionSelectedViewHelper("species by month", "speciesbymonth");
+
 				if ($this->getYear() == "" )
 				{
-					echo " | " . $this->linkToSelfChangeView("speciesbyyear", "by year"); 
+					$this->optionSelectedViewHelper("species by year", "speciesbyyear");
 				}
 			}
-			echo "<br/>";
-		}
+		} ?>
 
-		echo "photos: ";
-		echo $this->linkToSlideshow() . " | " . $this->linkToSelfChangeView("photo", "thumbnails");
+		<optgroup label="photos"> <?
+		   $this->optionSelectedViewHelper("thumbnails", "photo");
+		   $this->optionSelectedViewHelper("slideshow", "slideshow"); ?>
+		</optgroup>
 
-		?></div><?
+		?></select><? $this->linkToSlideshow(); ?></form><?
 	}
+
+	function globalMenuBirds()
+	{
+		if (! strstr(getenv("SCRIPT_NAME"), "species") &&
+			$this->getOrderID() == "" && $this->getFamilyID() == "" && $this->getSpeciesID() == "")
+		{ ?>
+			<div class="command-disabled"><a href="./speciesindex.php">birds</a></div<?
+		}
+		else
+		{
+?>			<div class="command"><a class="commandlink" href="./speciesindex.php">birds</a>
+<?
+			if ($this->getOrderID() != "")
+			{
+				$orderInfo = $this->getOrderInfo();
+				$orderRequest = new Request;
+				$orderRequest->setPageScript("orderdetail.php");
+				$orderRequest->setSpeciesID("");
+				$orderRequest->setFamilyID(""); ?>
+			
+				<div class="subcommand1"><?= $orderRequest->command($orderInfo["LatinName"]) ?></div>
+<?		    }
+		
+			if ($this->getFamilyID() != "")
+			{
+				$familyInfo = $this->getFamilyInfo();
+				
+				$familyRequest = new Request;
+				$familyRequest->setPageScript("familydetail.php");
+				$familyRequest->setSpeciesID(""); ?>
+					 
+				<div class="subcommand2"><?= $familyRequest->command($familyInfo["LatinName"]) ?></div>
+<?		    }
+
+			if ($this->getSpeciesID() != "")
+			{
+				$speciesInfo = $this->getSpeciesInfo();
+				
+				$speciesRequest = new Request;
+				$speciesRequest->setPageScript("speciesdetail.php"); ?>
+			
+				 <div class="subcommand3"><?= $speciesRequest->command($speciesInfo["CommonName"]) ?></div>
+<?		    }
+?>          </div><?
+		}
+	}
+
+
+	function globalMenuLocations()
+	{
+		if (! strstr(getenv("SCRIPT_NAME"), "location") &&
+			$this->getStateID() == "" && $this->getCounty() == "" && $this->getLocationID() == "")
+		{ ?>
+			<div class="command-disabled"><a href="./locationindex.php">locations</a></div><?
+		}
+		else
+		{
+?>			<div class="command"><a class="commandlink" href="./locationindex.php">locations</a>
+<?
+			if ($this->getStateID() != "")
+			{
+				$stateInfo = $this->getStateInfo();
+				$stateRequest = new Request;
+				$stateRequest->setPageScript("statedetail.php");
+				$stateRequest->setLocationID("");
+				$stateRequest->setCounty(""); ?>
+			
+				<div class="subcommand1"><?= $stateRequest->command($stateInfo["Name"]) ?></div>
+<?		    }
+		
+			if ($this->getCounty() != "")
+			{
+				$countyRequest = new Request;
+				$countyRequest->setPageScript("countydetail.php");
+				$countyRequest->setLocationID(""); ?>
+					 
+				<div class="subcommand2"><?= $countyRequest->command($this->getCounty() . " county") ?></div>
+<?		    }
+
+			if ($this->getLocationID() != "")
+			{
+				$locationInfo = $this->getLocationInfo();
+				$locationRequest = new Request;
+				$locationRequest->setPageScript("locationdetail.php"); ?>
+			
+				<div class="subcommand3"><?= $locationRequest->command($locationInfo["Name"]) ?></div>
+<?		    }
+?>          </div><?
+		}
+	}
+
+	function globalMenuTrips()
+	{
+		if (! strstr(getenv("SCRIPT_NAME"), "trip") &&
+			$this->getYear() == "" && $this->getMonth() == "" && $this->getTripID() == "")
+		{ ?>
+			<div class="command-disabled"><a href="./tripindex.php">trips</a></div><?
+		}
+		else
+		{
+?>			<div class="command">
+				<a class="commandlink" href="./tripindex.php">trips</a>
+<?
+			if ($this->getYear() != "")
+			{
+				$yearRequest = new Request;
+				$yearRequest->setPageScript("yeardetail.php");
+				$yearRequest->setTripID("");
+				$yearRequest->setMonth(""); ?>
+			
+				<div class="subcommand1"><?= $yearRequest->command($this->getYear()) ?></div>
+<?		    }
+		
+			if ($this->getMonth() != "")
+			{
+				$monthRequest = new Request;
+				$monthRequest->setPageScript("monthdetail.php");
+				$monthRequest->setTripID(""); ?>
+					 
+				<div class="subcommand2"><?= $monthRequest->command(getMonthNameForNumber($this->getMonth())) ?></div>
+<?		    }
+
+			if ($this->getTripID() != "")
+			{
+				$tripInfo = $this->getTripInfo();
+				$dayOfMonth = date("jS", strtotime($tripInfo["Date"]));
+				$tripRequest = new Request;
+				$tripRequest->setPageScript("tripdetail.php"); ?>
+			
+				<div class="subcommand3"<?= $tripRequest->command($dayOfMonth) ?></div>
+<?		    }
+?>          </div><?
+		}
+	}
+
+
+
+
+	function globalMenu()
+	{ ?>
+		<div class="topleft">
+		    <a href="./index.php">
+		        <img src="./images/logo.gif" border=0 width="50px" alt="birdWalker logo"><br/>
+		        <div>Birding field notes and photos from Bill Walker</div>
+		    </a>
+		</div>
+
+		<div class="contentleft">
+			
+<?          $this->globalMenuTrips(); ?>
+<?          $this->globalMenuBirds(); ?>
+<?          $this->globalMenuLocations(); ?>
+
+			<div class="command-disabled"><a href="./speciesindex.php?view=chrono">life list</a></div>
+	        <div class="command-disabled"><a href="./credits.php">about</a></div>
+
+<?	        if (getEnableEdit())
+	        { ?>
+	        <div class="command-disabled"><a href="./tripcreate.php">create trip</a></div>
+            <div class="command-disabled"><a href="./photosneeded.php">photo todo</a></div>
+	        <div class="command-disabled"><a href="./errorcheck.php">db todo</a></div>
+<?	        } ?>
+
+	        <div class="command-disabled"><a href="./indexrss.php">RSS</a></div>
+
+        </div>
+<?  }
+
 }
