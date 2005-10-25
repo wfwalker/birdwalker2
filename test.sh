ea00a3245f3c8306e75c14bb835a0950ef2d4f4d
walker <walker>
2005-10-25 16:05:01 +0000

    test speciesedit

--- test.sh
+++ test.sh
@@ -7,6 +7,7 @@ echo -n "" > testresults.txt
 baseURL="http://localhost/~walker/birdwalker2/"
 
 successURLs="\
+speciesedit.php?speciesid=9022020100 \
 sightingedit.php?sightingid=11631 \
 tripedit.php?tripid=430 \
 countyindex.php \
