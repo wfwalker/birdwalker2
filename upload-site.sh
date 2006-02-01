1b0c40922e46c2f9cc77232910cccc3db6848c26
walker <walker>
2006-02-01 17:34:29 +0000

    copy the backgnd image, copy almost all the php

--- upload-site.sh
+++ upload-site.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 
-filesToCopy=$(find . -maxdepth 1 -type f -name \*.php -not -name \*edit\.\* -and -not -name speciesindex2.php -and -not -name photosneeded.php -and -not -name targetyearbirds.php -and -not -name \*create\*)
+filesToCopy=$(find . -maxdepth 1 -type f -name \*.php -not -name \*edit\.\* -and -not -name \*create\*)
 
 scp stylesheet.css $filesToCopy walker@sven.spflrc.org:~/.www/
+scp images/left.jpg walker@sven.spflrc.org:~/.www/images/
