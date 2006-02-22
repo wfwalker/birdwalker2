7971bff2cf54f50063445d46374054414b9a9af9
walker <walker>
2006-02-22 23:39:41 +0000

    change uploader to account for new graphics

--- upload-site.sh
+++ upload-site.sh
@@ -3,4 +3,4 @@
 filesToCopy=$(find . -maxdepth 1 -type f -name \*.php -not -name \*edit\.\* -and -not -name \*create\*)
 
 scp stylesheet.css $filesToCopy walker@sven.spflrc.org:~/.www/
-scp images/left.jpg walker@sven.spflrc.org:~/.www/images/
+scp images/mapspot.gif images/topleft*.jpg images/*banner*.jpg walker@sven.spflrc.org:~/.www/images/
