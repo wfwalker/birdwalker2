31ec0bcaf805233ba9c6fec0da69f81317af07b7
walker <walker>
2006-03-25 01:29:43 +0000

    use the password correctly

--- build-db.sh
+++ build-db.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-cat birdwalker.sql state.sql trip.sql location.sql species.sql sighting.sql taxonomy.sql| /usr/local/mysql/bin/mysql -u birdwalker -p birdwalker
+cat birdwalker.sql state.sql trip.sql location.sql species.sql sighting.sql taxonomy.sql| /usr/local/mysql/bin/mysql -u birdwalker -pbirdwalker
 
 
 
