48a0aa377bc7c86e6bfce8815bb77d1654f40ec5
walker <walker>
2006-04-10 06:20:45 +0000

    update to mysqldump 5

--- dump-db.sh
+++ dump-db.sh
@@ -1,7 +1,13 @@
-mysqldump -u birdwalker -pbirdwalker birdwalker state | grep INSERT > state.sql
-mysqldump -u birdwalker -pbirdwalker birdwalker location | grep INSERT > location.sql
-mysqldump -u birdwalker -pbirdwalker birdwalker sighting | grep INSERT > sighting.sql
-mysqldump -u birdwalker -pbirdwalker birdwalker trip | grep INSERT > trip.sql
-mysqldump -u birdwalker -pbirdwalker birdwalker species | grep INSERT > species.sql
-mysqldump -u birdwalker -pbirdwalker birdwalker taxonomy | grep INSERT > taxonomy.sql
-mysqldump -u birdwalker -pbirdwalker birdwalker countyfrequency | grep INSERT > countyfrequency.sql
+#!/bin/bash
+
+EXECUTABLE="mysqldump"
+AUTHFLAGS="-u birdwalker -pbirdwalker birdwalker"
+OTHERFLAGS="--extended-insert=FALSE --quote-names=FALSE"
+
+$EXECUTABLE $OTHERFLAGS $AUTHFLAGS state | grep INSERT > state.sql
+$EXECUTABLE $OTHERFLAGS $AUTHFLAGS location | grep INSERT > location.sql
+$EXECUTABLE $OTHERFLAGS $AUTHFLAGS sighting | grep INSERT > sighting.sql
+$EXECUTABLE $OTHERFLAGS $AUTHFLAGS trip | grep INSERT > trip.sql
+$EXECUTABLE $OTHERFLAGS $AUTHFLAGS species | grep INSERT > species.sql
+$EXECUTABLE $OTHERFLAGS $AUTHFLAGS taxonomy | grep INSERT > taxonomy.sql
+$EXECUTABLE $OTHERFLAGS $AUTHFLAGS countyfrequency | grep INSERT > countyfrequency.sql
