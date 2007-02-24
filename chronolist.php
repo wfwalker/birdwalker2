9cbf64562f57118c6bae0ec69917a5182154ba42
walker <walker>
2007-02-24 07:34:35 +0000

    try image generation

--- chronolist.php
+++ chronolist.php
@@ -168,5 +168,67 @@ class ChronoList
 
 
 
+	function timelineGIF()
+	{
+		performQuery("Create Temp Table",
+		  "CREATE TEMPORARY TABLE tmp (
+            SpeciesAbbreviation varchar(16) default NULL,
+            TripDate date default NULL,
+            objectid varchar(16) default NULL);");
+
+		$imageWidth = 400;
+		$imageHeight = 250;
+		$timelineImage    = imagecreatetruecolor($imageWidth, $imageHeight);
+		$black    = imagecolorallocate($timelineImage, 0, 0, 0);
+		$gray    = imagecolorallocate($timelineImage, 128, 128, 128);
+		$white        = imagecolorallocate($timelineImage, 255, 255, 255);
+		imagefill($timelineImage, 0, 0, $white);
+		imagestring($timelineImage, 2, 5, 5, "Hello World!", $black);
+
+
+        // here's what section 3.6.4 of the mysql manual calls:
+        // "a quite inefficient trick called the MAX-CONCAT trick"
+		// TODO upgrade to mysql 4.1 and use a subquery
+		performQuery("Put Sightings into Temp Table",
+          "INSERT INTO tmp
+            SELECT SpeciesAbbreviation,
+              LEFT(        MIN( CONCAT(TripDate,lpad(sighting.objectid,6,'0')) ), 10) AS TripDate,
+              0+SUBSTRING( MIN( CONCAT(TripDate,lpad(sighting.objectid,6,'0')) ),  11) AS objectid ".
+					 $this->mSightingQuery->getFromClause() . " " . 
+					 $this->mSightingQuery->getWhereClause() . "  AND species.ABACountable='1' AND sighting.Exclude!='1'
+            GROUP BY SpeciesAbbreviation");
+
+		// TODO count rows in the first sightings table!
+		$minDays = performCount("first", "select min(to_days(TripDate)) from sighting");
+		$maxDays = performCount("first", "select max(to_days(TripDate)) from sighting");
+
+		$firstSightingQuery = performQuery("Choose first sightings",
+          "SELECT to_days(sighting.TripDate) as tripDays
+          FROM sighting, tmp, species, location, trip
+          WHERE
+             sighting.SpeciesAbbreviation=tmp.SpeciesAbbreviation AND
+             species.Abbreviation=sighting.SpeciesAbbreviation AND
+             sighting.TripDate=tmp.TripDate AND
+             location.Name=sighting.LocationName AND
+             trip.Date=sighting.TripDate
+          ORDER BY sighting.TripDate DESC, LocationName, species.objectid;");
+
+		$speciesCount = mysql_num_rows($firstSightingQuery);
+
+		$index = 1;
+	   while($sightingInfo = mysql_fetch_array($firstSightingQuery))
+	   {
+		 $x = $imageWidth * ($sightingInfo["tripDays"] - $minDays) / ($maxDays - $minDays);
+		 $index = $index + 1;
+		 $y = $imageHeight * $index / $speciesCount;
+		 imagerectangle($timelineImage, $x - 1, $y - 1, $x + 1, $y + 1, $black);
+		 imageline($timelineImage, $x, $y, $x, $imageHeight, $gray);
+	   }
+
+		performQuery("Remove temporary table", "DROP TABLE tmp;");
+
+		imagegif($timelineImage);
+		imagedestroy($timelineImage);
+    }
 }
 ?>
