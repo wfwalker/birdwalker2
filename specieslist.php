29a302776fe1d3ecf5b67ccf65990304e3950217
walker <walker>
2007-03-28 16:38:55 +0000

    just simple species list, thank you

--- specieslist.php
+++ specieslist.php
@@ -10,42 +10,20 @@ $request = new Request;
 
 $speciesQuery = new SpeciesQuery($request);
 
-htmlHead("thing");
+htmlHead($request->getPagetitle());
 $request->globalMenu();
 
 ?>
 
 
   <div id="topright-species">
-	  <div class="pagekind">Species Checklist</div>
+	  <div class="pagekind">Species List</div>
 	  <div class="pagetitle"> <?= $request->getPagetitle() ?></div>
 	  <div class="pagesubtitle"> </div>
   </div>
 
   <div id="contentright">
-	
-
-<?
-		  $dbQuery = $speciesQuery->performQuery();
-		  $divideByTaxo = mysql_num_rows($dbQuery) > 20;
-          $prevInfo = ""; 
-
-		  while($info = mysql_fetch_array($dbQuery))
-		  {
-			  if ($prevInfo == "" || ($divideByTaxo && ($prevInfo["objectid"]) != getFamilyInfo($info["objectid"])))
-			  {
-				  $taxoInfo = getFamilyInfo($info["objectid"]); ?>
-				  <div class="subheading"><?= strtolower($taxoInfo["LatinName"]) ?></div>
-<?            } ?>
-
-			  <div>
-                  <input type="checkbox" name="Abbreviations" value="<?= $info["Abbreviation"]?>"/>
-                  <?= $info["CommonName"] ?>
-              </div>
-<?            $prevInfo = $info;
-          }
- ?>
-
+<?    $speciesQuery->formatTwoColumnSpeciesList(); ?>
   </div>
 
 <?
