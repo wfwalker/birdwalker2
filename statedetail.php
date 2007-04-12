7479962722120e2a53fad0ca8a515980b2639623
walker <walker>
2007-04-12 01:26:55 +0000

    add link to biggest state day

--- statedetail.php
+++ statedetail.php
@@ -14,6 +14,14 @@ htmlHead($info["Name"]);
 
 $request->globalMenu();
 
+
+$biggestStateDay = performOneRowQuery("biggest state day",
+   "SELECT trip.*, year(TripDate) as year, DATE_FORMAT(TripDate, '%M') as month, COUNT(DISTINCT(sighting.SpeciesAbbreviation)) AS count
+      FROM sighting,trip,location
+      WHERE sighting.TripDate=trip.Date AND sighting.LocationName=location.Name AND location.State='" . $info["Abbreviation"] . "'
+      GROUP BY trip.Date
+      ORDER BY count DESC LIMIT 1");
+
 ?>
 
     <div id="topright-location">
@@ -23,6 +31,14 @@ $request->globalMenu();
 	</div>
 
     <div id="contentright">
+
+  <div class="heading">Biggest state day: <?= $biggestStateDay["month"]?> <?= $biggestStateDay["year"]?></div>
+  <div class="onecolumn">
+    <a href="./tripdetail.php?tripid=<?= $biggestStateDay["objectid"] ?>"><?= $biggestStateDay["Name"]; ?></a>, <?= $biggestStateDay["count"] ?> species
+  </div>
+
+
+
 <?
       $request->handleStandardViews();
       footer();
