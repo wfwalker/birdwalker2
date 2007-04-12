2db36136587f27bab7f49e80eaa5ab4e78b8b2f0
walker <walker>
2007-04-12 01:26:22 +0000

    add county big day link

--- countydetail.php
+++ countydetail.php
@@ -14,6 +14,13 @@ $request->globalMenu();
 $locationQuery = new LocationQuery($request);
 $stateInfo =  $request->getStateInfo();
 
+$biggestCountyDay = performOneRowQuery("biggest county day",
+   "SELECT trip.*, year(TripDate) as year, DATE_FORMAT(TripDate, '%M') as month, COUNT(DISTINCT(sighting.SpeciesAbbreviation)) AS count
+      FROM sighting,trip,location
+      WHERE sighting.TripDate=trip.Date AND sighting.LocationName=location.Name AND location.County='" . $request->getCounty() . "'
+      GROUP BY trip.Date
+      ORDER BY count DESC LIMIT 1");
+
 ?>
 
     <div id="topright-location">
@@ -24,6 +31,12 @@ $stateInfo =  $request->getStateInfo();
 	</div>
 
     <div id="contentright">
+
+  <div class="heading">Biggest county day: <?= $biggestCountyDay["month"]?> <?= $biggestCountyDay["year"]?></div>
+  <div class="onecolumn">
+    <a href="./tripdetail.php?tripid=<?= $biggestCountyDay["objectid"] ?>"><?= $biggestCountyDay["Name"]; ?></a>, <?= $biggestCountyDay["count"] ?> species
+  </div>
+
 <?
       $request->handleStandardViews();
       footer();
