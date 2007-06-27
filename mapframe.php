aac41513298bae9b3d6230b4de05f74ad8683e3d
walker <walker>
2007-06-27 05:56:18 +0000

    do less stuff in the header

--- mapframe.php
+++ mapframe.php
@@ -4,8 +4,19 @@ require_once("./birdwalker.php");
 require_once("./request.php");
 
 $request = new Request;
+echo "<!DOCTYPE  HTML PUBLIC  \"-//W3C//DTD HTML 4.01 Transitional//EN\">";
+?>
 
-htmlHead("Map");
+<html>
+  <head>
+    <link rel="alternate" type="application/atom+xml" title="Atom" href="./indexrss.php" />
+    <link rel="SHORTCUT ICON" href="./images/favicon.ico">
+    <link title="Style" href="./stylesheet.css" type="text/css" rel="stylesheet">
+    <title>birdWalker | <?= $title ?></title>
+  </head>
+
+  <body>
+<?
 
 $map = new Map("./" . $request->getPageScript(), $request);
 $map->drawGoogle(true);
